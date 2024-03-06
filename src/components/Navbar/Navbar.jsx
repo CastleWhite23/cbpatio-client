a7dd992bed20c8657249cc35fe74d74b8dd2c771
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-05 22:10:54 -0300

    adicionando o nav toogle e func para abrir e fechar a navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,13 +2,33 @@ import { Link } from "react-router-dom"
 import Logo from '../../assets/logo.png'
 import './Navbar.css'
 import { Button } from "../Button/Button"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { useState } from "react"
+
 
 const Navbar = ({ bgColor }) => {
+    const [navOpen, setNavOpen] = useState(false)
+    const handleToogleClick = () => {
+        const bgResponsive = document.querySelector('.bgResponsive')
+        const nav = document.querySelector('.navbar')
+        const btnLogin = document.querySelector('button.purple')
 
-    const linkStyle = {
-        color: '#fff',
-        fontWeight: '600',
+        if (!navOpen) {
+            bgResponsive.classList.remove('none')
+
+            nav.classList.add('flex')
+        } else {
+            bgResponsive.classList.add('none')
+            nav.classList.remove('flex')
+            nav.classList.add('none')
+            
+        }
+
+
+        setNavOpen(!navOpen)
     }
+
 
 
     return (
@@ -19,11 +39,14 @@ const Navbar = ({ bgColor }) => {
                 <div className="logo">
                     <Link to='/'><img src={Logo} alt="logo" /></Link>
                 </div>
-                <nav>
+                <div className="bgResponsive none"></div>
+                <FontAwesomeIcon icon={faBars} className="nav-toogle" onClick={handleToogleClick} />
+
+                <nav className="navbar">
                     <ul>
-                        <Link to='/campeonatos' style={linkStyle}>Campeonatos</Link>
-                        <Link to='/times' style={linkStyle}>Times</Link>
-                        <Link to='/classificacao' style={linkStyle}>Classificação</Link>
+                        <Link to='/campeonatos' className="linkStyle">Campeonatos</Link>
+                        <Link to='/times' className="linkStyle">Times</Link>
+                        <Link to='/classificacao' className="linkStyle">Classificação</Link>
                     </ul>
 
                 </nav>
@@ -33,6 +56,7 @@ const Navbar = ({ bgColor }) => {
                     width={'88px'}
                     padding={'1rem'}
                 />
+
             </header>
 
         </>
