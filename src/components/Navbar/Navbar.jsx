65b8ae774f665802cc1f7de946307bd458e1a5b9
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-07 21:30:46 -0300

    adicionando a funcionalidade do toggle e xmark icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,29 +3,15 @@ import Logo from '../../assets/logo.png'
 import './Navbar.css'
 import { Button } from "../Button/Button"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { useState } from "react"
 
 
 const Navbar = ({ bgColor }) => {
     const [navOpen, setNavOpen] = useState(false)
     const handleToogleClick = () => {
-        const bgResponsive = document.querySelector('.bgResponsive')
         const nav = document.querySelector('.navbar')
-        const btnLogin = document.querySelector('button.purple')
-
-        if (!navOpen) {
-            bgResponsive.classList.remove('none')
-
-            nav.classList.add('flex')
-        } else {
-            bgResponsive.classList.add('none')
-            nav.classList.remove('flex')
-            nav.classList.add('none')
-            
-        }
-
-
+        nav.classList.toggle("active")
         setNavOpen(!navOpen)
     }
 
@@ -39,14 +25,28 @@ const Navbar = ({ bgColor }) => {
                 <div className="logo">
                     <Link to='/'><img src={Logo} alt="logo" /></Link>
                 </div>
-                <div className="bgResponsive none"></div>
-                <FontAwesomeIcon icon={faBars} className="nav-toogle" onClick={handleToogleClick} />
+                {/* <div className="bgResponsive"></div> */}
+                {
+                    !navOpen ? (
+                        <FontAwesomeIcon icon={faBars} className="nav-toogle" onClick={handleToogleClick} />
+                    ) : (
+                        <FontAwesomeIcon icon={faXmark} className="nav-toogle" onClick={handleToogleClick} />
+                    )
+
+                }
+
 
                 <nav className="navbar">
                     <ul>
                         <Link to='/campeonatos' className="linkStyle">Campeonatos</Link>
                         <Link to='/times' className="linkStyle">Times</Link>
                         <Link to='/classificacao' className="linkStyle">Classificação</Link>
+                        <Button
+                            text={'Entrar'}
+                            variant={'purple'}
+                            width={'200px'}
+                            padding={'1rem'}
+                        />
                     </ul>
 
                 </nav>
