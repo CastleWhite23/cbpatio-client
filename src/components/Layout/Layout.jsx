de74ea034ccd03af278cd5aecb0c5b6dff68903f
zaiaaa <gustavo.0801214@gmail.com>
2024-03-20 17:48:00 -0300

    modificando o layout para que caso ele nao receba uma imagem de fundo, ele tenha um fundo de uma cor estática

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -6,6 +6,7 @@ const Layout = ({ children, bgImage = "", navStyle = "" }) => {
   const backgroundImage = typeof bgImage === "string" ? bgImage : "";
 
   return (
+    bgImage ?
     <div
       className="layout"
       style={{
@@ -14,7 +15,7 @@ const Layout = ({ children, bgImage = "", navStyle = "" }) => {
         minHeight: "100vh",
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
-        backgroundPosition: "center"
+        backgroundPosition: "center",
       }}
     >
 
@@ -26,6 +27,29 @@ const Layout = ({ children, bgImage = "", navStyle = "" }) => {
       </div>
 
     </div>
+
+    :
+
+    <div
+    className="layout"
+    style={{
+      backgroundColor: "#252848",
+      width: "100%",
+      minHeight: "100vh",
+      backgroundRepeat: "no-repeat",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+    }}
+  >
+
+    {
+      navStyle == "home" ? <Navbar /> : <Navbar bgColor={'#22243F'} />
+    }
+    <div className="content">
+      {children}
+    </div>
+
+  </div>
   );
 };
 
