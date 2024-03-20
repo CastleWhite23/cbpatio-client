1c9ee2dd17b01e180879ff426cca96e029f55245
zaiaaa <gustavo.0801214@gmail.com>
2024-03-20 17:49:36 -0300

    swiperCampeonatos para abrigar os campeonatos do servidor em seus respectivos cards em um carrossel de arrastar pro lado

---
+++ src/components/swiperCampeonatos/swiperCampeonatos.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react'
+import foto from "../../assets/bg_com_logo.png"
+
+import "./swiperCampeonatos.css"
+
+//  SWIPER
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import { Api } from '../../services/Api';
+import { CardCampeonato } from '../cardCampeonato/cardCampeonato';
+
+const path = "http://localhost:3005"
+
+const SwiperCampeonatos = () => {
+  
+    const [campeonatos, setCampeonatos] = useState([])
+
+    useEffect(() => {
+        const getCampeonatos = async () =>{
+            const {data: campeonatos} = await Api.get('/campeonatos')
+            setCampeonatos(campeonatos)
+        }
+
+        getCampeonatos()
+    }, [])
+
+    
+  
+  return (
+    <div>
+        <Swiper 
+            slidesPerView={1}
+            autoplay={{
+                delay: 3000, // tempo de intervalo em milissegundos (3 segundos)
+                disableOnInteraction: true // para continuar a reprodução automática após interações do usuário
+            }}
+            centeredSlides={false}
+            spaceBetween={5}
+            navigation={true}
+            modules={[Pagination, Navigation, Autoplay]}
+            breakpoints={{
+                768: {
+                    slidesPerView: 2,
+                    spaceBetween: 10
+                },
+                1268: {
+                    slidesPerView: 3,
+                    spaceBetween: 20
+                }
+            }}
+            className='mySwiper'
+        >
+
+        {
+            campeonatos.length === 0
+            
+            ?
+            
+            "Não há campeonatos para o momento."
+
+            :
+
+            campeonatos.map((campeonato, index) => {
+                return (<SwiperSlide key={index}>
+                    {console.log(`${path}/${campeonato.foto}`)}
+                        <CardCampeonato idCamp={campeonato.id_campeonato} bgImage={`${path}/${campeonato.foto.replace(/\\/g, '/')}`} title={campeonato.nome} height={"25rem"} width={"100%"}/>
+                </SwiperSlide>)
+            })
+        }
+
+        </Swiper>
+    </div>
+  )
+}
+
+export {SwiperCampeonatos}
