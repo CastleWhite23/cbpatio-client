04cfcdbdf3f8b9d6169f9cdd3c3b74d457803ab2
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 15:34:53 -0300

    retirando as responsabilidades a mais desnecessarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,85 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import bgImage from './assets/bg_escuro_emba.png'
-import { Layout } from './components/Layout/Layout'
+
 import { ChakraProvider } from '@chakra-ui/react'
-import { Home } from './pages/Home/Home'
-import { Campeonatos } from './pages/Campeonatos/Campeonatos'
-import {Solicitacoes} from './pages/Solicitacoes/Solicitacoes'
-import {NovoTime} from './pages/NovoTime/NovoTime'
-import {MeusTimes} from './pages/MeusTimes/MeusTimes'
+import { MainRoutes } from './components/MainRoutes/MainRoutes'
+
 // import { PrivateRoute } from './services/privateRoute'
 // import { AuthContext, AuthContextProvider } from './context/auth'
 // import { Login } from './pages/login/login'
 
 function App() {
   return (
-    <Router>
-      {/* <AuthContextProvider> */}
-      <ChakraProvider>
-        <Routes>
-          {/* ROTAS QUE NÃO PRECISAM DE LOGIN */}
-          <Route path='/' element={
-            <Layout bgImage={bgImage} navStyle='home'>
-              <Home />
-            </Layout>
-
-          } />
-          <Route path='/campeonatos' element={
-            <Layout>
-              <Campeonatos />
-            </Layout>
-          } />
-
-          {/* ROTAS DE LOGIN E CADASTRO */}
-          <Route path='/login' element={
-            <Layout>
-              <h1>login</h1>
-            </Layout>
-          } />
-
-          <Route path='/cadastro' element={
-            <Layout>
-              <h1>cadastro</h1>
-            </Layout>
-          } />
-
-          {/* ROTAS QUE O LOGIN É OBRIGÁTORIO */}
-
-
-          {/* ROTAS TIMES */}
-
-          <Route path='/times/criar' element={
-            <Layout>
-              <NovoTime />
-            </Layout>
-          } />
-
-          <Route path='/times/meustimes' element={
-            <Layout>
-              <MeusTimes />
-            </Layout>
-          } />
-
-          <Route path='/times/solicitacoes' element={
-            <Layout>
-              <Solicitacoes />
-            </Layout>
-          } />
-
-
-
-
-
-
-          <Route path='/classificacao' element={
-            <Layout>
-              <Campeonatos />
-            </Layout>
-          } />
-        </Routes>
-      </ChakraProvider>
-      {/* </AuthContextProvider> */}
-    </Router>
+    <ChakraProvider>
+      {/* AuthContextProvider */}
+      <MainRoutes />
+    </ChakraProvider>
   )
 }
 
