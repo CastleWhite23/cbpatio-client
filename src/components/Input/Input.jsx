56584683872b7092736d6aae0b4d7e43f89f686c
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 18:04:04 -0300

    criando e estilizando o componnete input

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Controller } from 'react-hook-form'
+import "./Input.css"
+
+const Input = ({defaultValue, name, control, disabled, readOnly, type = 'text',...rest}) => {
+  return (
+    <>        
+        <Controller name={name}
+        control={control}
+        defaultValue={type == 'file' ? null : defaultValue}
+        rules={{required: true}}
+        render={({field: {value, onChange}}) => type == 'file' ? 
+        //se for um input de file
+        <input
+        type={type}
+        onChange={(e) => onChange(e.target.files[0])}
+        disabled={disabled}
+        readOnly={readOnly}
+        {...rest}
+      />
+      //se for um input de data
+      : type == 'datetime-local' ? <input
+      type={type}
+      defaultValue={defaultValue}
+      onChange={(e) => onChange(e.target.value)}
+      disabled={disabled}
+      readOnly={readOnly}
+      {...rest}
+      /> 
+      
+      //se não for um input de file nem de data
+      : <input className='input' type={type} defaultValue={defaultValue} onChange={onChange} disabled={disabled} readOnly={readOnly} {...rest}/>}
+        />
+    </>
+  )
+}
+
+
+export {Input}
