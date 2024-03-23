39616b53f04643ad77c68cc973d7457cd398c184
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 15:26:59 -0300

    criando um componente para a pagina de novo time

---
+++ src/pages/MeusTimes/MeusTimes.jsx
@@ -0,0 +1,11 @@
+import { Times } from "../Times/Times"
+
+const MeusTimes = () =>{
+    return(
+        <Times pageTitle={'MEUS TIMES'}>
+            conteudo dessa pagina
+        </Times>
+    )
+}
+
+export {MeusTimes}
