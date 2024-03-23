4fe01c9c31a0e6e28fd77b7e8308ecc6d72f5729
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 13:34:21 -0300

    criando rotas de login e rotas dos times, bem como importando o componente com o layout da pagina de times (falta a navbar)

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout/Layout'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Home } from './pages/Home/Home'
 import { Campeonatos } from './pages/Campeonatos/Campeonatos'
+import { Times } from './pages/Times/Times'
 // import { PrivateRoute } from './services/privateRoute'
 // import { AuthContext, AuthContextProvider } from './context/auth'
 // import { Login } from './pages/login/login'
@@ -27,14 +28,52 @@ function App() {
             </Layout>
           } />
 
-
-
-          {/* ROTAS QUE O LOGIN É OBRIGÁTORIO */}
-          <Route path='/times' element={
+          {/* ROTAS DE LOGIN E CADASTRO */}
+          <Route path='/login' element={
             <Layout>
-              <Campeonatos />
+              <h1>login</h1>
             </Layout>
           } />
+
+          <Route path='/cadastro' element={
+            <Layout>
+              <h1>cadastro</h1>
+            </Layout>
+          } />
+
+          {/* ROTAS QUE O LOGIN É OBRIGÁTORIO */}
+
+
+          {/* ROTAS TIMES */}
+          <Route path='/times' element={
+            <Layout>
+              <Times />
+            </Layout>
+          } />
+
+          <Route path='/times/criar' element={
+            <Layout>
+              <h1>criar</h1>
+            </Layout>
+          } />
+
+          <Route path='/times/meustimes' element={
+            <Layout>
+              <h1>meus times</h1>
+            </Layout>
+          } />
+
+          <Route path='/times/solicitacoes' element={
+            <Layout>
+              <h1>solicitações</h1>
+            </Layout>
+          } />
+
+
+
+
+
+
           <Route path='/classificacao' element={
             <Layout>
               <Campeonatos />
