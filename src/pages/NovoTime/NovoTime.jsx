893f553dedb54798bc37584d3d4d276660544644
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 15:27:17 -0300

    criando um componente para a pagina de meus times

---
+++ src/pages/NovoTime/NovoTime.jsx
@@ -0,0 +1,11 @@
+import { Times } from "../Times/Times"
+
+const NovoTime = () =>{
+    return(
+        <Times pageTitle={'NOVO TIME'}>
+            conteudo dessa pagina
+        </Times>
+    )
+}
+
+export {NovoTime}
