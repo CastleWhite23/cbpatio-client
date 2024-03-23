19d2d18911490a927db9f8662711d2091b001355
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-23 13:36:58 -0300

    deixando o button inscreva-se do home  funcional

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,23 @@
 
 import './Home.css'
 import { Button } from '../../components/Button/Button'
+import { Link } from 'react-router-dom'
 const Home = () => {
     return (
         <>
             <div className="home">
                 <h1>CBPatio E-Sports </h1>
                 <p>Prepare-se para uma experiência única de jogo! O CBPatio Arena é o palco virtual onde a emoção acontece em tempo real. </p>
-                <Button
-                    text={'Inscreva-se'}
-                    variant={'purple'}
-                    width={'200px'}
-                    padding={'1rem'}
-                />
+                <Link
+                    to={'/campeonatos'}
+                >
+                    <Button
+                        text={'Inscreva-se'}
+                        variant={'purple'}
+                        width={'200px'}
+                        padding={'1rem'}
+                    />
+                </Link>
 
             </div>
         </>
