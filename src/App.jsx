d7fed99ac0e4ce83c477fb3249efe461d8d55608
zaiaaa <gustavo.0801214@gmail.com>
2024-03-25 19:49:44 -0300

    colocando o context provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { MainRoutes } from './components/MainRoutes/MainRoutes'
 
 // import { PrivateRoute } from './services/privateRoute'
-// import { AuthContext, AuthContextProvider } from './context/auth'
+import { AuthContext, AuthContextProvider } from './context/context'
 // import { Login } from './pages/login/login'
 
 function App() {
   return (
     <ChakraProvider>
-      {/* AuthContextProvider */}
-      <MainRoutes />
+      <AuthContextProvider>
+        <MainRoutes />
+      </AuthContextProvider>
     </ChakraProvider>
   )
 }
