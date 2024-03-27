f1a9f3e0a8b3d9c511d31f666b0df9d1e8a24b98
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-03-26 22:44:34 -0300

    identando código KKKKK, brincadeira

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { Button } from "../Button/Button"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons"
 import { useContext, useEffect, useState } from "react"
-import {AuthContext} from "../../context/context"
+import { AuthContext } from "../../context/context"
 import foto from "../../assets/logo.png"
 
 
@@ -18,14 +18,14 @@ const Navbar = ({ bgColor }) => {
         setNavOpen(!navOpen)
     }
 
-    const {isAuth, getUserData} = useContext(AuthContext)
+    const { isAuth, getUserData } = useContext(AuthContext)
 
     useEffect(() => {
         console.log(isAuth)
     }, [isAuth])
 
     console.log(`${path}/fotoUsuarios/sem_foto_user.png`)
-    
+
 
     return (
         <>
@@ -52,55 +52,55 @@ const Navbar = ({ bgColor }) => {
                         <Link to='/times/meustimes' className="linkStyle">Times</Link>
                         <Link to='/classificacao' className="linkStyle">Classificação</Link>
                         {
-                            isAuth 
+                            isAuth
 
-                            ?
-                            
-                            <Link to={'/login'}>
-                                <Button
-                                    text={getUserData().nome}
-                                    variant={'purple'}
-                                    width={'200px'}
-                                    padding={'1rem'}
-                                />
-                            </Link>
-                            
-                            :
+                                ?
 
-                            
-                            <Link to={'/login'}>
-                            <Button
-                                text={'Entrar'}
-                                variant={'purple'}
-                                width={'200px'}
-                                padding={'1rem'}
-                            />
-                            </Link>
+                                <Link to={'/login'}>
+                                    <Button
+                                        text={getUserData().nome}
+                                        variant={'purple'}
+                                        width={'200px'}
+                                        padding={'1rem'}
+                                    />
+                                </Link>
+
+                                :
+
+
+                                <Link to={'/login'}>
+                                    <Button
+                                        text={'Entrar'}
+                                        variant={'purple'}
+                                        width={'200px'}
+                                        padding={'1rem'}
+                                    />
+                                </Link>
                         }
 
                     </ul>
 
                 </nav>
-                
+
                 {
                     isAuth
 
-                    ?
-                    <Link to={"/config"}>
-                        <div id="user">
-                            <p>{ getUserData().nome }</p>
-                            <img src={getUserData()?.foto ? `${path}/${getUserData()?.foto.replace(/\\/g, '/')}` : `${path}/fotoUsuarios/sem_foto_user.png`} alt="" srcset="" />
-                        </div>
-                    </Link>
-                    :
-                    <Link to={'/login'}>
-                        <Button
-                            text={'Entrar'}
-                            variant={'purple'}
-                            width={'88px'}
-                            padding={'1rem'}
-                        />
-                    </Link>
+                        ?
+                        <Link to={"/config"}>
+                            <div id="user">
+                                <p>{getUserData().nome}</p>
+                                <img src={getUserData()?.foto ? `${path}/${getUserData()?.foto.replace(/\\/g, '/')}` : `${path}/fotoUsuarios/sem_foto_user.png`} alt="" srcset="" />
+                            </div>
+                        </Link>
+                        :
+                        <Link to={'/login'}>
+                            <Button
+                                text={'Entrar'}
+                                variant={'purple'}
+                                width={'88px'}
+                                padding={'1rem'}
+                            />
+                        </Link>
                 }
 
 
