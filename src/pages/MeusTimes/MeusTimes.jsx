d24be6ca81665955cb7b9169b4dd5fd45053d43c
zaiaaa <gustavo.0801214@gmail.com>
2024-03-27 12:26:43 -0300

    colocando os componentes na pagina de Times

--- src/pages/MeusTimes/MeusTimes.jsx
+++ src/pages/MeusTimes/MeusTimes.jsx
@@ -1,9 +1,16 @@
+import { DividerComponent } from "../../components/Divider/DividerComponent"
+import { MeusTimesComponent } from "../../components/meusTimes/meusTimesComponent"
+import { PageTitle } from "../../components/pageTitle/pageTitle"
 import { Times } from "../Times/Times"
 
 const MeusTimes = () =>{
     return(
         <Times pageTitle={'MEUS TIMES'}>
-            conteudo dessa pagina
+            <DividerComponent />
+            <PageTitle text={"CAPITÃO"} />
+            <MeusTimesComponent />
+            <PageTitle text={"JOGADOR"} />
+            <MeusTimesComponent />
         </Times>
     )
 }
