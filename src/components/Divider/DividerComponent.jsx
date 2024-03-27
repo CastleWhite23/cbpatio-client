48474be1bfa0bead393d324b614b059ef415f3d2
zaiaaa <gustavo.0801214@gmail.com>
2024-03-27 12:24:39 -0300

    criando um componente de Divider

---
+++ src/components/Divider/DividerComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Divider } from "@chakra-ui/react";
+
+const DividerComponent = () => {
+  return (
+    <Divider border={"1px solid #580AFF"} margin={"10px 0"}/>
+  )
+}
+
+export {DividerComponent}
