a2dbedebf4ef3663f661c256988c4d5017e3bc80
zaiaaa <gustavo.0801214@gmail.com>
2024-03-27 12:24:24 -0300

    adicionando props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Button.css"
 
-const Button = ({type, variant, text, width, padding, margin, onClick}) => {
+const Button = ({type, variant, text, width, padding, margin, onClick, height, fontSize}) => {
   return (
     <div className='ct-btn'>
-      <button className={`button ${variant}`} type={type} style={{width, padding, margin}} onClick={onClick}>
+      <button className={`button ${variant}`} type={type} style={{width, padding, margin, height, fontSize}} onClick={onClick}>
           {text}
       </button>
     </div>
