d02cef4e1e343a031419299db7f3839522cccb42
=CastleWhite23 <=pedrocastelobrancodosantos@gmail.com>
2024-04-03 12:27:01 -0300

    colocando a função para adicionar e remover a classe

--- src/components/SidebarTimes/SidebarTimes.jsx
+++ src/components/SidebarTimes/SidebarTimes.jsx
@@ -5,6 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faSquarePlus, faUsers } from "@fortawesome/free-solid-svg-icons"
 
 const SidebarTimes = () => {
+    const handleActive = (e) => {
+        e.currentTarget.classList.add('active')
+        console.log(e)
+        
+    }
+
     return (
         <>
 
@@ -15,15 +21,15 @@ const SidebarTimes = () => {
                 {/* <Link to='/'>
                     user08
                 </Link> */}
-                <Link to='/times/criar'>
+                <Link to='/times/criar' id='sideLink' onClick={handleActive}>
                 <FontAwesomeIcon icon={faSquarePlus}/>
                     <span>Novo Time</span>
                 </Link>
-                <Link to='/times/meustimes'>
-                <FontAwesomeIcon icon={faUsers}/>
+                <Link to='/times/meustimes' id='sideLink'onClick={handleActive}>
+                <FontAwesomeIcon icon={faUsers} />
                     <span>Meus Times</span>
                 </Link>
-                <Link to='/times/solicitacoes'>
+                <Link to='/times/solicitacoes' id='sideLink' onClick={handleActive}>
                     <FontAwesomeIcon icon={faBell}/>
                     <span>Solicitações</span>
                 </Link>
