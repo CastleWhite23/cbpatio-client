46a91147299e526bdb0ce1b9b3d83c22d0f6db51
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-06 15:37:26 -0300

    colocando o button do time que o usuario é capitão dentro de um link dinamico

--- src/components/meusTimes/meusTimesComponent.jsx
+++ src/components/meusTimes/meusTimesComponent.jsx
@@ -1,44 +1,57 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { Button } from '../Button/Button'
 import { Api } from '../../services/Api'
-import {AuthContext} from "../../context/context"
+import { AuthContext } from "../../context/context"
+import { Link } from 'react-router-dom'
+import { SpinnerCustom } from '../Spinner/Spinner'
 
-const MeusTimesComponent = ({cargo}) => {
-  
+
+const MeusTimesComponent = ({ cargo }) => {
+
     const [timesCapitao, setTimesCapitao] = useState([])
-    const {getUserData} = useContext(AuthContext)
+    const { getUserData } = useContext(AuthContext)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
 
         const getTimes = async () => {
-            const {data: capitao} = await Api.get(`/times/time/capitao/${getUserData().id}`)
+            const { data: capitao } = await Api.get(`/times/time/capitao/${getUserData().id}`)
             setTimesCapitao(capitao)
+            setLoading(false)
         }
         getTimes()
     }, [])
 
-    console.log(timesCapitao) 
+    console.log(timesCapitao)
 
-  return (
-    <div>
-        {
-            cargo == "capitao" 
+    return (
+        <div>
+            {
+                cargo == "capitao" ?
+                    (
+                        !loading ?
 
-            ?
+                        (
+                            timesCapitao.map((time) => {
+                                return (
+                                    <Link to={`/times/meustimes/capitao/${time.id_time}`}>
+                                        <Button text={`${time.nome} - Capitão`} variant={"purple"} margin={"10px 0"} height={"4rem"} fontSize={"20px"} />
+                                    </Link>
+                                )
+                            })
+                        )
+                        :
+                        (
+                            <SpinnerCustom />
+                        )
+                    )
 
+                    :
 
-            timesCapitao.map((time) => {
-                return(
-                    <Button text={`${time.nome} - Capitão`} variant={"purple"} margin={"10px 0"} height={"4rem"} fontSize={"20px"} />
-                )
-            })
-
-            :
-
-            ""
-        }
-    </div>
-  )
+                    ""
+            }
+        </div>
+    )
 }
 
-export {MeusTimesComponent}
+export { MeusTimesComponent }
