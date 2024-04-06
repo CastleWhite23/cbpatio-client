6aa68fe924c97123dedac7654615cb6c90db4602
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-06 15:31:51 -0300

    adicionando opção de ter icone do font Awesome no pagTitle

--- src/components/pageTitle/pageTitle.jsx
+++ src/components/pageTitle/pageTitle.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./pageTitle.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const PageTitle = ({text}) => {
+const PageTitle = ({text, icon}) => {
   return (
-    <h1 className='pgTitle'>{text}</h1>
+    <h1 className='pgTitle'>{icon && <FontAwesomeIcon icon={icon} />} {text} </h1>
+    
   )
 }
 
