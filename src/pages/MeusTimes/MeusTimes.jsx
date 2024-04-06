decd41d498292f3432b33b69ba271ca2a82f6302
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-06 15:30:54 -0300

    adicionando icones nos titulos de pagina do meusTimes

--- src/pages/MeusTimes/MeusTimes.jsx
+++ src/pages/MeusTimes/MeusTimes.jsx
@@ -2,18 +2,19 @@ import { DividerComponent } from "../../components/Divider/DividerComponent"
 import { MeusTimesComponent } from "../../components/meusTimes/meusTimesComponent"
 import { PageTitle } from "../../components/pageTitle/pageTitle"
 import { Times } from "../Times/Times"
+import { faCrown, faChessKnight } from "@fortawesome/free-solid-svg-icons"
 
-const MeusTimes = () =>{
-    return(
+const MeusTimes = () => {
+    return (
         <Times pageTitle={'MEUS TIMES'}>
-            <DividerComponent margin={"10px 0"}/>
-            <PageTitle text={"CAPITÃO"} />
-            <MeusTimesComponent cargo={"capitao"}/>
-            <DividerComponent margin={"3rem 0 0 1rem"}/>
-            <PageTitle text={"JOGADOR"} />
+            <DividerComponent margin={"10px 0"} />
+            <PageTitle text={`CAPITÃO `} icon={faCrown} />
+            <MeusTimesComponent cargo={"capitao"} />
+            <DividerComponent margin={"3rem 0 0 1rem"} />
+            <PageTitle text={"JOGADOR"} icon={faChessKnight}/>
             <MeusTimesComponent />
         </Times>
     )
 }
 
-export {MeusTimes}
+export { MeusTimes }
