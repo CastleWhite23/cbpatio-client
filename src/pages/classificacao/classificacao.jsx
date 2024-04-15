097c7c0128f95776aa156b474b98dc69591b6224
zaiaaa <gustavo.0801214@gmail.com>
2024-04-15 12:23:44 -0300

    terminando os jogos ativos e mostrando-os na tela (ainda falta eliminado e campeão)

--- src/pages/classificacao/classificacao.jsx
+++ src/pages/classificacao/classificacao.jsx
@@ -10,6 +10,8 @@ import { AuthContext } from '../../context/context'
 import { Card } from '../../components/Card/Card'
 import { CardCampeonato } from '../../components/cardCampeonato/cardCampeonato'
 
+const path = "http://localhost:3005"
+
 const Classificacao = () => {
 
     const [liveOn, setLiveOn] = useState(false)
@@ -61,30 +63,31 @@ const Classificacao = () => {
 
                 </div>
                 {games.map((game) => (
-                    <>
-                    <CardCampeonato idCamp={1}>
+                    <div className='camp__jogo'>
 
-                    </CardCampeonato>
+                        <CardCampeonato idCamp={1} bgImage={`${path}/${game.foto.replace(/\\/g, '/')}`}>
 
-                    <Card variant={"darkpurple"} width={"40%"}>
-                        <h1>JOGO {game.jogo} - {game.fase}</h1>
-                        <DividerComponent />
-                        <div>
-                            <span>{game.data_hora}</span>
+                        </CardCampeonato>
 
-                            <div>
-                                <h2>{game.nome_time}</h2>
-                                <span>VS.</span>
-                                <h2>{game.nome_time_vs}</h2>
-                            </div>
-
-                            <span>COMEÇA ÀS {game.data_hora}</span>
-
+                        <Card variant={"darkpurple"} width={"40%"}>
+                            <h1>JOGO {game.jogo} - {game.fase}</h1>
                             <DividerComponent />
-                            <h1></h1>
-                        </div>
-                    </Card>
-                    </>
+                            <div>
+                                <span>{game.data_hora}</span>
+
+                                <div>
+                                    <h2>{game.nome_time}</h2>
+                                    <span>VS.</span>
+                                    <h2>{game.nome_time_vs}</h2>
+                                </div>
+
+                                <span>COMEÇA ÀS {game.data_hora}</span>
+
+                                <DividerComponent />
+                                <h1></h1>
+                            </div>
+                        </Card>
+                    </div>
                 ))}
                 
             </div>
