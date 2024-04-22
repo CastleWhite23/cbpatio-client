1fd88f0400959f105818995651aa1b5ab0c623ef
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-21 21:39:34 -0300

    adicionando funções de formatar data e formatar hora

--- src/services/getData.js
+++ src/services/getData.js
@@ -13,3 +13,38 @@ export const getData = () =>{
     
     return dataCriacao;
 }
+
+
+export const formataData = (data) =>{
+
+    const dataAtual = new Date(data);
+
+    const ano = dataAtual.getFullYear();
+    const mes = dataAtual.getMonth() + 1; // Os meses começam do zero, então você precisa adicionar 1
+    const dia = dataAtual.getDate();
+    const hora = dataAtual.getHours();
+    const minuto = dataAtual.getMinutes();
+    const segundo = dataAtual.getSeconds();
+    let mesFinal = mes
+    mes < 10 ? mesFinal = `0${mes}` : '';
+
+    const dataCriacao = `${dia}/${mesFinal}/${ano}`;
+    
+    return dataCriacao;
+}
+
+export const formataHora = (data) =>{
+
+    const dataAtual = new Date(data);
+
+    const ano = dataAtual.getFullYear();
+    const mes = dataAtual.getMonth() + 1; // Os meses começam do zero, então você precisa adicionar 1
+    const dia = dataAtual.getDate();
+    const hora = dataAtual.getHours();
+    let minuto = dataAtual.getMinutes();
+    minuto == 0 ? minuto = '00':'';
+    const segundo = dataAtual.getSeconds();
+    const dataCriacao = `${hora}:${minuto}`;
+    
+    return dataCriacao;
+}
