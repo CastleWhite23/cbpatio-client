f93225d7ec2598e4c1c615d3574c52619d5b5e4a
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-21 21:40:10 -0300

    colocando uma verificação aqui para saber mostrar ou não o burton para se inscrever

--- src/components/cardCampeonato/cardCampeonato.jsx
+++ src/components/cardCampeonato/cardCampeonato.jsx
@@ -4,18 +4,22 @@ import { Card } from '../Card/Card'
 import { Link } from 'react-router-dom'
 import "./cardCampeonato.css"
 
-const CardCampeonato = ({idCamp, title, width, height, bgImage}) => {
+const CardCampeonato = ({ idCamp, title, width, height, bgImage, type }) => {
   return (
     <Card width={width} height={height} variant={'img'} bgImage={bgImage}>
-            <h3>{title}</h3>
-            
-            <Link to={`/campeonatos/inscrever/${idCamp}`}>
-                <Button text={"Inscreva-se"} variant={"purple"} width={"100%"} />
-            </Link>
+      <h3>{title}</h3>
+
+      {
+        type !== 'preview' &&
+          <Link to={`/campeonatos/inscrever/${idCamp}`}>
+            <Button text={"Inscreva-se"} variant={"purple"} width={"100%"} />
+          </Link>
+      }
+
     </Card>
 
-                
+
   )
 }
 
-export {CardCampeonato}
+export { CardCampeonato }
