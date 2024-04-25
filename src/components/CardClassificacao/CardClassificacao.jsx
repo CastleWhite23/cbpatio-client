2d060860cdfb0a794f5db9f07a94c157c71bdebf
--replace-all <pedrocastelobrancodosantos@gmail.com>
2024-04-25 09:19:42 -0300

    atualizando e colocando verificações corretas no cardClassificação

--- src/components/CardClassificacao/CardClassificacao.jsx
+++ src/components/CardClassificacao/CardClassificacao.jsx
@@ -4,50 +4,83 @@ import { formataData, formataHora } from '../../services/getData'
 
 import './CardClassificacao.css'
 
-const CardClassificacao = ({jogo, data_hora, fase, nome_time, nome_time_vs}) => {
+const CardClassificacao = ({ jogo, data_hora, fase, nome_time, nome_time_vs, eliminado = false, eliminado_em = "", campeao = false }) => {
 
 
-    return(
+    return (
+        campeao ? (
+            <Card variant={"darkpurple"} width={"40%"}>
+                <div className='card-main'>
+                    <div>
+                        <h2 className='greenlight'>{nome_time}</h2>
+                    </div>
+                </div>
+                <DividerComponent />
+                <div className='card-footer'>
+                    <h1 className="green">{"Campeão"}</h1>
+                </div>
+            </Card>
+
+        ) : eliminado ? (
+            <Card variant={"darkpurple"} width={"40%"}>
+                <div className='card-main'>
+                    <span className='darkpurple'>{
+                        `Já aconteceu em ${formataData(data_hora)}`
+                    }
+                    </span>
+
+                </div>
+                <DividerComponent />
+                <div className='card-footer'>
+                    <h1 className={
+                        (eliminado_em.slice(9)) == ' final' ? 'green' :
+                            (eliminado_em.slice(9)) == ' semis' ? 'pink' :
+                                (eliminado_em.slice(9)) == ' quartas' ? 'orange' :
+                                    (eliminado_em.slice(9)) == ' oitavas' && 'blue'
+                    }>{eliminado_em.slice(9)} - <span className='red'>Eliminado</span></h1>
+                </div>
+            </Card>
+        ) :
 
 
-                            <Card variant={"darkpurple"} width={"40%"}>
-                                <div className='card-header'>
-                                    <h1>JOGO {jogo}</h1>
-                                </div>
-                                <DividerComponent />
-                                <div className='card-main'>
-                                    <span className='darkpurple'>{
-                                        data_hora != null ?
-                                            `Vai acontecer em ${formataData(data_hora)}` :
-                                            "Data ainda não definida!"
-                                    }
-                                    </span>
+            <Card variant={"darkpurple"} width={"40%"}>
+                <div className='card-header'>
+                    <h1>JOGO {jogo}</h1>
+                </div>
+                <DividerComponent />
+                <div className='card-main'>
+                    <span className='darkpurple'>{
+                        data_hora != null ?
+                            `Vai acontecer em ${formataData(data_hora)}` :
+                            "Data ainda não definida!"
+                    }
+                    </span>
 
-                                    <div>
-                                        <h2 className='greenlight'>{nome_time}</h2>
-                                        <span>VS.</span>
-                                        <h2 className='red'>{nome_time_vs}</h2>
-                                    </div>
+                    <div>
+                        <h2 className='greenlight'>{nome_time}</h2>
+                        <span>VS.</span>
+                        <h2 className='red'>{nome_time_vs}</h2>
+                    </div>
 
-                                    <span className='darkpurple'>{
-                                        data_hora != null ?
-                                            `Vai começar às ${formataHora(data_hora)}` :
-                                            "Hora ainda não definida!"
-                                    }
-                                    </span>
-                                </div>
-                                <DividerComponent />
-                                <div className='card-footer'>
-                                    <h1 className={
-                                        fase == 'final' ? 'green' :
-                                            fase == 'semis' ? 'pink' :
-                                                fase == 'quartas' ? 'orange' :
-                                                    fase == 'oitavas' && 'blue'
-                                    }>{fase}</h1>
-                                </div>
-                            </Card>
-                        
+                    <span className='darkpurple'>{
+                        data_hora != null ?
+                            `Vai começar às ${formataHora(data_hora)}` :
+                            "Hora ainda não definida!"
+                    }
+                    </span>
+                </div>
+                <DividerComponent />
+                <div className='card-footer'>
+                    <h1 className={
+                        fase == 'final' ? 'green' :
+                            fase == 'semis' ? 'pink' :
+                                fase == 'quartas' ? 'orange' :
+                                    fase == 'oitavas' && 'blue'
+                    }>{fase}</h1>
+                </div>
+            </Card>
+
     )
 }
 
-export {CardClassificacao}
+export { CardClassificacao }
