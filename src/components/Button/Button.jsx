bc013b622b1553870c6334b09549fd6c388be73f
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-26 21:17:22 -0300

    adicionando props de border radius

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Button.css"
 
-const Button = ({type, variant, text, width, padding, margin, onClick, height, fontSize, disabled}) => {
+const Button = ({type, variant, text, width, padding, margin, onClick, height, fontSize, disabled, borderRadius}) => {
   return (
     <div className='ct-btn'>
-      <button className={`button ${variant}`} type={type} disabled={disabled} style={{width, padding, margin, height, fontSize}} onClick={onClick}>
+      <button className={`button ${variant}`} type={type} disabled={disabled} style={{width, padding, margin, height, fontSize, borderRadius}} onClick={onClick}>
           {text}
       </button>
     </div>
