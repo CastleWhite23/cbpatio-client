c0a58a97d7c0878b48bb4e28d8f434243ef119ed
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-26 22:13:59 -0300

    retirando o campeonato descritpition do form stepper

--- src/components/FormStepper/FormStepper.jsx
+++ src/components/FormStepper/FormStepper.jsx
@@ -4,9 +4,11 @@ import { decodeHashId } from '../../services/formatFunctions'
 
 import { StepperCustom } from '../../components/StepperCustom/StepperCustom'
 import { Button } from '../../components/Button/Button'
+import { CampeonatoDescription } from '../../components/CampeonatoDescription/CampeonatoDescription'
+import './FormStepper.css'
 
 
-const FormStepper = ({component, indexStep}) => {
+const FormStepper = ({ component, indexStep }) => {
     const { id_camp } = useParams()
     const id = decodeHashId(id_camp)
     console.log(id)
@@ -14,7 +16,13 @@ const FormStepper = ({component, indexStep}) => {
 
     return (
         <>
-            {component}
+            <div className="form-stepper">
+                <div className='components'>
+                    
+                    {component}
+                </div>
+
+            </div>
             <StepperCustom indexStep={indexStep} />
 
         </>
