2f903eca6acefe9835a02a43e7836200b83ff81a
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-26 21:18:01 -0300

    usando o form stepper na pagina de inscirção

--- src/pages/Inscrever/inscrever.jsx
+++ src/pages/Inscrever/inscrever.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+
 import { useParams } from 'react-router-dom'
 import { decodeHashId } from '../../services/formatFunctions'
 import { InscreverEquipe } from '../../components/InscreverEquipe/inscreverEquipe'
+import { FormStepper } from '../../components/FormStepper/FormStepper'
+
 
 const Inscrever = () => {
-    const {id_camp} = useParams()
+    const { id_camp } = useParams()
     const id = decodeHashId(id_camp)
-    console.log(id)
+
+
 
     return (
-        <InscreverEquipe id_campeonato={id}/>
+        <>
+            <h1>Campeonato description</h1>
+
+           <FormStepper component={ <InscreverEquipe id_campeonato={id} />} indexStep={0}/>
+
+        </>
+
     )
 }
 
-export {Inscrever}
+export { Inscrever }
