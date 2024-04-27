a6cf6574fd999df5740ea35467edba5f41dc2e2d
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-27 12:56:59 -0300

    adicionando os textos no campeonato description e retirando o card

--- src/components/CampeonatoDescription/CampeonatoDescription.jsx
+++ src/components/CampeonatoDescription/CampeonatoDescription.jsx
@@ -5,20 +5,32 @@ import logo from '../../assets/logo.png'
 const CampeonatoDescription = ({ id_camp }) => {
     return (
         <div className="camp-descr">
-            <Card variant={"lightpurple"} width={"100%"} >
+
+            <div className="logo">
                 <img src={logo} alt="" />
-                <h1>DETALHES DO CAMPEONATO</h1>
-                <h3>Sobre: asdf</h3>
-                <h3>Datas: asdf</h3>
-                <h3>Quem pode increver um time: asdf</h3>
-                <h3>Limite de jogadores: asdf</h3>
-                <h3>Forma de pagamento: asdf</h3>
-                <h3>Premiação: asdf</h3>
-                <h3>Caso de desistencia e/ou atraso: asdf</h3>
-            </Card>
+                <h1>AVISOS DO
+                    <br />CAMPEONATO</h1>
+            </div>
+
+            <div className='descricao'>
+                <h3><span className='tit-descricao'>Datas: </span> As inscrições deste campeonato vão até <span className="descri-destaque">xx/xx/xxxx</span>, e o campeonato será streamado em <span className="descri-destaque">xx/xx/xxxx</span></h3>
+
+                <h3><span className='tit-descricao'>Quem pode increver um time:</span> Você precisa ser um <span className='descri-destaque'>capitão</span> para inscrever um time e realizar o pagamento.</h3>
+
+                <h3><span className='tit-descricao'>Limite de jogadores:</span> : Este campeonato só aceita times com<span className="descri-destaque"> xx integrante(s)</span>. Os limites de jogadores são dinamicamente alocados para o campeonato.</h3>
+
+                <h3><span className='tit-descricao'>Forma de pagamento:</span>  Por enquanto o CBPATIO só aceita pagamentos por <span className="descri-destaque">PIX.</span> Estamos trabalhando em atualizações do gênero (em breve aceitaremos moedas de <span className="descri-destaque">ouro & prata</span>)</h3>
+
+                <h3><span className='tit-descricao'>Premiação:</span> A premiação do campeonato  é de <span className="descri-destaque">150,00 R$.</span> Será pago via <span className="descri-destaque">PIX</span> pelos organizadores, então deixe seu <span className="descri-destaque">contato em dia.</span></h3>
+
+                <h3><span className='tit-descricao'>Caso de desistencia e/ou atraso:</span>  Você tem <span className="descri-destaque">10 (DEZ) minutos de tolerância</span> para entrar no jogo e deixar tudo pronto. Caso não cumpra com o horário, você receberá <span className="descri-destaque">W.O</span> e será <span className="descri-destaque">eliminado</span>.</h3>
+            </div>
+
+
         </div>
 
     )
+
 }
 
 export { CampeonatoDescription }
