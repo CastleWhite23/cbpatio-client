f92aa15e5dd5a69603b43b6a1d8bab3825dac3df
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-30 13:01:25 -0300

    retirando o hanlde active pq tem conflito com link e por isso que tem que clicar duas vezes para funcionar

--- src/components/SidebarTimes/SidebarTimes.jsx
+++ src/components/SidebarTimes/SidebarTimes.jsx
@@ -1,15 +1,11 @@
 import './SidebarTimes.css'
-import { Link } from 'react-router-dom'
+import { Link, } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBell, faSquarePlus, faUsers } from "@fortawesome/free-solid-svg-icons"
 
 const SidebarTimes = () => {
-    const handleActive = (e) => {
-        e.currentTarget.classList.add('active')
-        console.log(e)
-        
-    }
+
 
     return (
         <>
@@ -21,16 +17,19 @@ const SidebarTimes = () => {
                 {/* <Link to='/'>
                     user08
                 </Link> */}
-                <Link to='/times/criar' id='sideLink' onClick={handleActive}>
-                <FontAwesomeIcon icon={faSquarePlus}/>
+
+                <Link to='/times/criar' id='sideLink' >
+                    <FontAwesomeIcon icon={faSquarePlus} />
                     <span>Novo Time</span>
                 </Link>
-                <Link to='/times/meustimes' id='sideLink'onClick={handleActive}>
-                <FontAwesomeIcon icon={faUsers} />
+
+
+                <Link to='/times/meustimes' id='sideLink' >
+                    <FontAwesomeIcon icon={faUsers} />
                     <span>Meus Times</span>
                 </Link>
-                <Link to='/times/solicitacoes' id='sideLink' onClick={handleActive}>
-                    <FontAwesomeIcon icon={faBell}/>
+                <Link to='/times/solicitacoes' id='sideLink' >
+                    <FontAwesomeIcon icon={faBell} />
                     <span>Solicitações</span>
                 </Link>
             </div>
