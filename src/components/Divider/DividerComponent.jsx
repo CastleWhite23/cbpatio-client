ef8ec4142d8f53b8c168d67ff542b0e604bb6419
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-04-30 12:57:21 -0300

    deixando o divider responsivo com largura automatica

--- src/components/Divider/DividerComponent.jsx
+++ src/components/Divider/DividerComponent.jsx
@@ -3,7 +3,7 @@ import { Divider } from "@chakra-ui/react";
 
 const DividerComponent = ({margin}) => {
   return (
-    <Divider border={"1px solid #580AFF"} margin={margin}/>
+    <Divider border={"1px solid #580AFF"} margin={margin} width={'auto'}/>
   )
 }
 
