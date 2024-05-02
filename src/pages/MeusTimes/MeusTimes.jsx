91551ef32ff439f5a6c4d8ff041d1ad2316106cb
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-05-01 22:07:01 -0300

    retirando um divider e arrumando um negocio das margens aqui

--- src/pages/MeusTimes/MeusTimes.jsx
+++ src/pages/MeusTimes/MeusTimes.jsx
@@ -7,10 +7,9 @@ import { faCrown, faChessKnight } from "@fortawesome/free-solid-svg-icons"
 const MeusTimes = () => {
     return (
         <Times pageTitle={'MEUS TIMES'}>
-            <DividerComponent margin={"10px 0"} />
             <PageTitle text={`CAPITÃO `} icon={faCrown} />
             <MeusTimesComponent cargo={"capitao"} />
-            <DividerComponent margin={"3rem 0 0 1rem"} />
+            <DividerComponent margin={"3rem 0  0  0"} />
             <PageTitle text={"JOGADOR"} icon={faChessKnight}/>
             <MeusTimesComponent cargo={'jogador'}/>
         </Times>
