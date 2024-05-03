0be5c5279d039c68f54f5f8e50b3b9e6435d592e
zaiaaa <gustavo.0801214@gmail.com>
2024-05-02 23:02:37 -0300

    arrumando instancias da api hospedada no railway

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client'
 
 
-const socket = io("https://cbpatio.onrender.com/", {
+const socket = io("https://cbpatio-production.up.railway.app/", {
   transports: ['websocket'],
 });
 
