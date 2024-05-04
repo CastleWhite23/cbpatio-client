e68772d8d4771df9e71598bd4be07673a2b083da
zaiaaa <gustavo.0801214@gmail.com>
2024-05-04 15:44:33 -0300

    arrumando o componente de live

--- src/components/liveOn/liveOn.jsx
+++ src/components/liveOn/liveOn.jsx
@@ -4,7 +4,7 @@ import "./liveOn.css"
 const LiveOn = () => {
   return (
     <div className='div__liveon'>
-        <iframe src="https://player.twitch.tv/?channel=cbpatio&parent=www.g1.com" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+        <iframe src="https://player.twitch.tv/?channel=cbpatio&parent=https://cbpatio.vercel.app" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
     </div>
   )
 }
