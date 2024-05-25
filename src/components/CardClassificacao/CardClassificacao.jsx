4a1914956d85a04062d59cd1d5efff103e5da642
zaiaaa <gustavo.0801214@gmail.com>
2024-05-25 19:00:22 -0300

    arrumei finally

--- src/components/CardClassificacao/CardClassificacao.jsx
+++ src/components/CardClassificacao/CardClassificacao.jsx
@@ -49,7 +49,19 @@ const CardClassificacao = ({ ocorrendo = false, jogo, data_hora, fase, nome_time
                             (eliminado_em.slice(9)) == ' semis' ? 'pink' :
                                 (eliminado_em.slice(9)) == ' quartas' ? 'orange' :
                                     (eliminado_em.slice(9)) == ' oitavas' && 'blue'
-                    }>{eliminado_em.slice(9)} - <span className='red'>Eliminado</span></h1>
+                    }>
+                    {
+                        eliminado_em.slice(9) != 'final'
+                        ?
+                        <>
+                            <p>{eliminado_em.slice(9)}</p> 
+                            <span className='red'>Eliminado</span>
+                        </>
+                        :
+                        <span className='red'>Vice-campeão</span>
+                    }    
+
+                    </h1>
                 </div>
             </Card>
         ): ocorrendo ? (
