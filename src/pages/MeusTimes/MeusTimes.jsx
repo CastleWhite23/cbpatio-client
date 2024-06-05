3f44621f1f5eb6e20c97bdcd6c4d24b31fa78834
CastleWhite23 <pedrocastelobrancodosantos@gmail.com>
2024-06-05 19:17:49 -0300

    adicionando um aviso

--- src/pages/MeusTimes/MeusTimes.jsx
+++ src/pages/MeusTimes/MeusTimes.jsx
@@ -3,12 +3,21 @@ import { MeusTimesComponent } from "../../components/meusTimes/meusTimesComponen
 import { PageTitle } from "../../components/pageTitle/pageTitle"
 import { Times } from "../Times/Times"
 import { faCrown, faChessKnight } from "@fortawesome/free-solid-svg-icons"
+import { Link } from "react-router-dom"
 
 const MeusTimes = () => {
     return (
         <Times pageTitle={'MEUS TIMES'}>
             <PageTitle text={`CAPITÃO `} icon={faCrown} />
             <MeusTimesComponent cargo={"capitao"} />
+            <div className="alerta">
+                    <p>
+                        Você pode <span className="span-cap">criar</span> no máximo <span className="span-cap">10 times</span>
+                    </p>
+                    <p>
+                        Você pode criar seus times clicando em <span className="span-cap"><Link to={'/times/criar'}>Novo Time</Link></span>.
+                    </p>
+                </div>
             <DividerComponent margin={"3rem 0  0  0"} />
             <PageTitle text={"JOGADOR"} icon={faChessKnight}/>
             <MeusTimesComponent cargo={'jogador'}/>
